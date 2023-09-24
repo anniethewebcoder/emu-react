4c661f251e1db79a973e401529a7b2e1661c1893
anniethewebcoder <annie.the.web.coder@gmail.com>
2023-09-23 20:32:13 -0400

    modified 404.html

---
+++ src/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import NotFound from './components/NotFound';
+
+const root = ReactDOM.createRoot(document.getElementById('notfound'))
+
+root.render(
+  <React.StrictMode>
+    <NotFound />
+  </React.StrictMode>
+  
+);
