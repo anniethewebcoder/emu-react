81261c68abb169358d3988448bf990e663a4cf66
anniethewebcoder <annie.the.web.coder@gmail.com>
2023-09-28 18:49:22 -0400

    about page styling

--- src/components/About.js
+++ src/components/About.js
@@ -9,14 +9,16 @@ const About = () => {
             <div className={styles.section__title}>
                 About Me
             </div>
-            <div className={styles.section__body}>
-                <p >
-                    <img src={profile} alt="profile" width="200" style={{float: "left", clear: "left", marginRight: "1em"}}/>
-                </p>
-                <p>    
-                    Hello, I'm Annie! I'm currently a student at Code the Dream where I can learn full stack web development. My favorite hobbies are being a board game enthusiast, an amateur photography,  a meme queen, a casual cozy reader, a sci-fi TV series consumer, and an occasional napper.   
-                </p>        
-                <p><Link to="/">Go back to home</Link></p>
+            <div className={styles.section__about}>
+                <div>
+                    <p><img src={profile} alt="profile" width="200"/></p>
+                </div>
+                <div>
+                    <p>    
+                        Hello, I'm Annie! I'm currently a student at Code the Dream where I can learn full stack web development. My favorite hobbies are being a board game enthusiast, an amateur photography,  a meme queen, a casual cozy reader, a sci-fi TV series consumer, and an occasional napper.
+                    </p>
+                    <p><Link to="/">Go back to home</Link></p>
+                </div>    
             </div>
         </div>
     )
